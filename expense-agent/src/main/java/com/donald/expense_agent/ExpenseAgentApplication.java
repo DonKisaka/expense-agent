c0da793dca4e97c48356e189f3184710690f0ba0
DonaldKisaka <davidkisaka4@gmail.com>
2026-05-04 12:39:27 +0300

    Initial commit

---
+++ expense-agent/src/main/java/com/donald/expense_agent/ExpenseAgentApplication.java
@@ -0,0 +1,13 @@
+package com.donald.expense_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseAgentApplication.class, args);
+	}
+
+}
