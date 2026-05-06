7732673fd53441a07d34ac8637722b4d32f8b7f5
DonaldKisaka <davidkisaka4@gmail.com>
2026-05-06 12:03:56 +0300

    Added the agent-service class

--- expense-agent/src/main/java/com/donald/expense_agent/service/AgentService.java
+++ expense-agent/src/main/java/com/donald/expense_agent/service/AgentService.java
@@ -4,7 +4,6 @@ import com.donald.expense_agent.tools.ExpenseTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.stereotype.Service;
